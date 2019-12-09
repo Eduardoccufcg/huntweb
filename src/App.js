6354422574957b15a0dbe25c656d4fd2e73c8903
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-12-09 15:28:18 -0300

    Aplicação implementada

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './componentes/Header'
+import Routes from './routes'
+import './styles.css'
+
+const App = () => (
+	<div className="App">
+		<Header></Header>
+		<Routes />
+	</div>
+
+)
+export default App;
